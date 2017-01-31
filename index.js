21322a29013a0b25977aca7a6dd1f164695b55bf
Hammam94 <gentle_engineer@hotmail.com>
2017-02-01 00:09:23 +0200

    write results to file

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var urlParse= require('url-parse');
+var fileSystem = require('fs-extra');
 
 var urls = [], visited = [];
 var root = new node();
-var consMaxDepth = 2;// max depth of tree
- 
+var consMaxDepth = 4;// max depth of tree
+var fileWriter, filename;
 
-//init the root of the tree
+
 function init() {
   root.url = process.argv[2];
+  if(process.argv.length > 4) consMaxDepth = process.argv[4];
   root.depth=1;
+  filename = process.argv[3];
+  if(!fileSystem.existsSync(filename)){
+    fileSystem.createFileSync(filename);
+    console.log("not created");
+  } 
+  fileWriter = fileSystem.createWriteStream(filename);
+  requests(root);
 }
 
 //create the node
@@ -32,11 +41,7 @@ function getAllPageLinksForTag($, tagName, attribute, path){
     if(typeof $(this).attr(attribute) != "undefined" && $(this).attr(attribute).indexOf("http") == 0){
       urls.push($(this).attr(attribute));
     } else if(typeof $(this).attr(attribute) != "undefined" && !($(this).attr(attribute).indexOf("#") == 0)){
-      if(path[path.length - 1] != '/'){
-        urls.push(path + '/' + $(this).attr(attribute));
-      }else{
-        urls.push(path + $(this).attr(attribute));
-      }
+      path[path.length - 1] != '/'? urls.push(path + '/' + $(this).attr(attribute)): urls.push(path + $(this).attr(attribute));
     }
   });
 }
@@ -51,7 +56,8 @@ function traverse(currentNode) {
     childNode.url = currentNode.childrn[i];
     childNode.depth= currentNode.depth+1;
     visited[childNode.url] = true;
-    console.log(childNode.url);
+    var u = childNode.url + "\n";
+    fileWriter.write(u);
     requests(childNode);
   }
 }
@@ -77,4 +83,3 @@ function requests(currentNode){
 
 // main
 init();
-requests(root);
